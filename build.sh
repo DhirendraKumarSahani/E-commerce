2d9533ec884bd0cc68c7e281012c812ee70f4d02
Dhirendra Kumar <dhirendraroy8882414182@gmail.com>
2026-03-30 18:02:52 +0530

    Fix superuser for phone-based auth

--- build.sh
+++ build.sh
@@ -11,18 +11,17 @@ python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
+if not User.objects.filter(phone="8882414182").exists():
 
     print("🔥 First time setup: deleting all users...")
 
     User.objects.all().delete()
 
     admin = User.objects.create_superuser(
-        username="DhirendraKumar",
-        password="####ROY@@@@."
+        phone="8882414182",
+        password="admin123"
     )
 
-    admin.phone = "8882414182"
     admin.email = "dhirendraroy8882414182@gmail.com"
     admin.is_staff = True
     admin.is_superuser = True
