21ca669207bcfc12d8a7120b12f48383365c37c8
Dhirendra Kumar <dhirendraroy8882414182@gmail.com>
2026-03-31 08:54:42 +0530

    Switch to Neon PostgreSQL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -115,11 +115,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
